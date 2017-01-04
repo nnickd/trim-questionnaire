532c05ef3dabce49df6d1c704df88e81bfb85974
nnickd <nicolas.dellitalia@gmail.com>
2017-01-04 12:39:31 -0500

    set up questionnaires controller, views and routes for show & new

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,2 +1,35 @@
 class QuestionnairesController < ApplicationController
+
+  def show
+    set_questionnaire
+  end
+
+  def new
+    @questionnaire = Questionnaire.new
+  end
+
+  def create
+  @questionnaire = Questionnaire.new(questionnaire_params)
+
+  respond_to do |format|
+    if @questionnaire.save
+      format.html { redirect_to @questionnaire, notice: 'Questionnaire was successfully created.' }
+      format.json { render :show, status: :created, location: @questionnaire }
+    else
+      format.html { render :new }
+      format.json { render json: @questionnaire.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+  private
+    def set_questionnaire
+      @questionnaire = Questionnaire.find(params[:id])
+    end
+
+    def questionnaire_params
+      params.require(:questionnaire).permit(:name, :email, :project_repo, :project_url, :oop, :modular, :fullstack,
+                                            :testing, :database, :debugging, :problem, :javascript, :html, :css,
+                                            :team, :motivation, :communication, :energy, :aptitude)
+    end
 end
