6844285be38370f928e61b3322a43e2da4bd7eaf
nnickd <nicolas.dellitalia@gmail.com>
2017-01-05 16:41:50 -0500

    if number input is empty change to 0

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,2 +1,9 @@
 class Questionnaire < ApplicationRecord
+  validates :name, :email, :project_repo, :project_url, presence: true
+
+  def points_used
+    oop + modular + fullstack + testing + database + debugging + problem +
+    javascript + html + css + team + motivation + communication + energy + aptitude
+  end
+
 end
