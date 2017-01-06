ed6fa2d9aa0311fc81424c58d85bca3fde3519a3
nnickd <nicolas.dellitalia@gmail.com>
2017-01-06 15:16:48 -0500

    style show view

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,5 +1,5 @@
 class QuestionnairesController < ApplicationController
-
+  
   def show
     set_questionnaire
   end
@@ -9,27 +9,28 @@ class QuestionnairesController < ApplicationController
   end
 
   def create
-  @questionnaire = Questionnaire.new(questionnaire_params)
+    @questionnaire = Questionnaire.new(questionnaire_params)
 
-  respond_to do |format|
-    if @questionnaire.save
-      format.html { redirect_to @questionnaire, notice: 'Questionnaire was successfully created.' }
-      format.json { render :show, status: :created, location: @questionnaire }
-    else
-      format.html { render :new }
-      format.json { render json: @questionnaire.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @questionnaire.save
+        format.html { redirect_to @questionnaire, notice: 'Questionnaire was successfully created.' }
+        format.json { render :show, status: :created, location: @questionnaire }
+      else
+        format.html { render :new }
+        format.json { render json: @questionnaire.errors, status: :unprocessable_entity }
+      end
     end
-  end
 end
 
   private
-    def set_questionnaire
-      @questionnaire = Questionnaire.find(params[:id])
-    end
 
-    def questionnaire_params
-      params.require(:questionnaire).permit(:name, :email, :project_repo, :project_url, :oop, :modular, :fullstack,
-                                            :testing, :database, :debugging, :problem, :javascript, :html, :css,
-                                            :team, :motivation, :communication, :energy, :aptitude)
-    end
+  def set_questionnaire
+    @questionnaire = Questionnaire.find(params[:id])
+  end
+
+  def questionnaire_params
+    params.require(:questionnaire).permit(:name, :email, :project_repo, :project_url, :oop, :modular, :fullstack,
+                                          :testing, :database, :debugging, :problem, :javascript, :html, :css,
+                                          :team, :motivation, :communication, :energy, :aptitude)
+  end
 end
